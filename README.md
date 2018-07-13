a3dfdf3b52886809cc054c0f604825530aebb87f
azu <azuciao@gmail.com>
2018-07-13 21:42:01 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# expected-exit-status
+
+A CLI check that command exit status is expected.
+
+- Cross platform(Windows/Linux/macOS) support
+- Test command's exit code
+
+## Install
+
+Install with [npm](https://www.npmjs.com/):
+
+    npm install expected-exit-status
+
+## Usage
+
+    Usage
+      $ expected-exit-status <status-code> --command <command>
+
+    Options:
+      --command Execute command
+
+    Examples
+      $ expected-exit-status 1 --command "exit 1"
+      // $? => 0
+      $ expected-exit-status 0 --command "exit 1"
+      // $? => 1
+
+## Changelog
+
+See [Releases page](https://github.com/azu/expected-exit-status/releases).
+
+## Running tests
+
+Install devDependencies and Run `npm test`:
+
+    npm i -d && npm test
+
+## Contributing
+
+Pull requests and stars are always welcome.
+
+For bugs and feature requests, [please create an issue](https://github.com/azu/expected-exit-status/issues).
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## Author
+
+- [github/azu](https://github.com/azu)
+- [twitter/azu_re](https://twitter.com/azu_re)
+
+## License
+
+MIT © azu
---
+++ bin/cmd.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+"use strict";
+const meow = require("meow");
+const expectedExitStatus = require("../lib/expected-exit-status.js").expectedExitStatus;
+
+const cli = meow(
+    `
+    Usage
+      $ expected-exit-status <status-code> --command <command>
+
+    Options:
+      --command Execute command
+
+    Examples
+      $ expected-exit-status 1 --command "exit 1"
+      // $? => 0
+      $ expected-exit-status 0 --command "exit 1"
+      // $? => 1
+`,
+    {
+        flags: {
+            command: {
+                type: "string"
+            }
+        },
+        autoVersion: true,
+        autoHelp: true
+    }
+);
+
+if (!cli.flags.command) {
+    cli.showHelp();
+}
+
+const exitStatusComparable = cli.input[0];
+if (exitStatusComparable === undefined) {
+    cli.showHelp();
+}
+const exitStatusNumber = Number(exitStatusComparable);
+if (!Number.isInteger(exitStatusNumber)) {
+    console.error("exit status should be integer");
+    cli.showHelp();
+}
+const result = expectedExitStatus(cli.flags.command, exitStatusNumber);
+if (!result.ok) {
+    console.log(`Expected exit status: ${exitStatusNumber}, but actual exit status is ${result.status}`);
+    console.log(result.stdout);
+    console.error(result.stderr);
+    process.exit(1);
+}
+process.exit(0);
