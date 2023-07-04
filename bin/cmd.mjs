776b97c01f562b0d27fdab9a11d38f97b59e99f0
azu <azuciao@gmail.com>
2023-07-04 21:19:53 +0900

    feat: support --stdout and --stderr testing

--- bin/cmd.mjs
+++ bin/cmd.mjs
@@ -1,50 +1,78 @@
 #!/usr/bin/env node
 "use strict";
-import meow from "meow";
+import { parseArgs } from "node:util";
 import { expectedExitStatus } from "../lib/expected-exit-status.js";
 
-const cli = meow(
-    `
+const USAGE = `
     Usage
       $ expected-exit-status <status-code> --command <command>
 
     Options:
       --command Execute command
+      --stdout  Expected stdout. "pattern" or "/pattern/"
+      --stderr  Expected stderr. "pattern" or "/pattern/"
 
     Examples
+      # exit status test -> It is ok
       $ expected-exit-status 1 --command "exit 1"
-      // $? => 0
+      // $? => 0 -> It throws error
       $ expected-exit-status 0 --command "exit 1"
-      // $? => 1
-`,
-    {
-        flags: {
-            command: {
-                type: "string"
-            }
+      # stdout test -> It is ok
+      $ expected-exit-status 0 --command "echo 'foo' && exit 1" --stdout "foo"
+      # stderr test -> It is ok
+      $ expected-exit-status 1 --command "echo 'err' >&2 && exit 1" --stderr "err"
+`
+
+const {
+    values,
+    positionals
+} = parseArgs({
+    allowPositionals: true,
+    options: {
+        command: {
+            type: "string"
         },
-        importMeta: import.meta,
-        autoVersion: true,
-        autoHelp: true
+        stdout: {
+            type: "string"
+        },
+        stderr: {
+            type: "string"
+        },
+        help: {
+            type: "boolean",
+            alias: "h"
+        }
     }
-);
+});
 
-if (!cli.flags.command) {
-    cli.showHelp();
+const showHelp = () => {
+    console.log(USAGE);
+    process.exit(2);
 }
-
-const exitStatusComparable = cli.input[0];
+if (!values.command) {
+    showHelp();
+}
+const exitStatusComparable = positionals[0];
 if (exitStatusComparable === undefined) {
-    cli.showHelp();
+    showHelp();
 }
 const exitStatusNumber = Number(exitStatusComparable);
 if (!Number.isInteger(exitStatusNumber)) {
     console.error("exit status should be integer");
-    cli.showHelp();
+    showHelp();
 }
-const result = expectedExitStatus(cli.flags.command, exitStatusNumber);
+const result = expectedExitStatus({
+    command: values.command,
+    expected: {
+        exitStatus: exitStatusNumber,
+        stdout: values.stdout,
+        stderr: values.stderr,
+    },
+});
 if (!result.ok) {
-    console.log(`Expected exit status: ${exitStatusNumber}, but actual exit status is ${result.status}`);
+    result.errors.forEach(error => {
+        console.error(error);
+    });
     process.exit(1);
 }
 process.exit(0);
